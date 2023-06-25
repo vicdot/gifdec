cb6f2ccff24340a8f4c908f8118a5ad0460b2755
c0052890Newcastle <c0052890@newcastle.ac.uk>
2023-06-25 11:53:28 +0100

    Add global color table function

--- gifdec.h
+++ gifdec.h
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <math.h>
+#include <bitset>
 
 typedef struct GifHeader
 {
@@ -13,15 +15,38 @@ typedef struct GifHeader
     uint8_t pixelAspectRatio;
 };
 
-typedef struct GlobalColorTable
+typedef struct ApplicationExtension
 {
 
 };
 
-void parse_header(std::ifstream *ifs, GifHeader* header)
+typedef struct CommentExtension
 {
-    printf("%s", "HEADER\n");
-    int len = 3;
+
+};
+
+typedef struct GraphicControlExtension
+{
+
+};
+
+typedef struct GlobalColorTable
+{
+    uint8_t* red;
+    uint8_t* green;
+    uint8_t* blue;
+
+    GlobalColorTable(int len)
+    {
+        red = new uint8_t[len];
+        green = new uint8_t[len];
+        blue = new uint8_t[len];
+    }
+    GlobalColorTable(){}
+};
+
+void parse_header(std::ifstream* ifs, GifHeader* header)
+{
     ifs->read(reinterpret_cast<char*>(&header->signature), 3);
     ifs->read(reinterpret_cast<char*>(&header->version), 3);
     ifs->read(reinterpret_cast<char*>(&header->screenWidth), 2);
@@ -29,12 +54,53 @@ void parse_header(std::ifstream *ifs, GifHeader* header)
     ifs->read(reinterpret_cast<char*>(&header->packedFields), 1);
     ifs->read(reinterpret_cast<char*>(&header->backgroundColorIndex), 1);
     ifs->read(reinterpret_cast<char*>(&header->pixelAspectRatio), 1);
-
 }
 
-void parse_colortable(std::ifstream *ifs, GlobalColorTable *gct)
+void get_colortable(std::ifstream* ifs, GlobalColorTable* gct, uint8_t gctSize)
 {
+    int len = pow(2,gctSize+1);
+    *gct = GlobalColorTable(len);
+    unsigned char* buffer;
+    buffer = new unsigned char[3*len];
 
+    ifs->read((char*)buffer, 3*len);
+    std::cout << "buffer?:\n";
+    for(int i = 0; i < 3*len; i++)
+    {
+        std::cout << std::hex << (int)buffer[i] << " ";
+        if((i+1) % 3 == 0)
+            std::cout << '\n';
+    }
+    for(int i = 0; i < len; i++)
+    {
+        gct->red[i] = buffer[(3*i)];
+        gct->green[i] = buffer[(3*i)+1];
+        gct->blue[i] = buffer[(3*i)+2];
+    }
+    /*
+    for(int i = 0; i < len; i++)
+    {
+        std::cout << "r:" << (int)gct->red[i] << " g:" << (int)gct->green[i]
+        << " b:" << (int)gct->blue[i] << '\n';
+    }
+    */
+    delete[] buffer;
+}
+
+void get_appextension(std::ifstream* ifs)
+{
+    unsigned char c[1];
+    ifs->read((char*)c, 1);
+    char appIdentifier[8];
+    char appAuthCode[3];
+    ifs->read(appIdentifier, 8);
+    ifs->read(appAuthCode, 3);
+    ifs->read((char*)c, 1);
+    std::cout << ifs->tellg();
+    if((int)c[0] > 1)
+    {
+        char
+    }
 }
 
 void file_handle(std::string fileName)
@@ -49,8 +115,24 @@ void file_handle(std::string fileName)
     std::size_t size = ifs.tellg();
     ifs.seekg(0, std::ios_base::beg);
     parse_header(&ifs, &header);
+
     bool check = header.packedFields & (1 << 0);
     if(check)
-        parse_colortable(&ifs, &gct);
+    {
+        std::cout << "yes" << '\n';
+        get_colortable(&ifs, &gct, header.packedFields & 0x7);
+    }
+    unsigned char c[2];
+    ifs.read((char*)c, 2);
+    if(c[0] == 0x21)
+    {
+
+        switch(c[1])
+        {
+            case 0xff:
+                get_appextension(&ifs);
+            break;
+        }
+    }
     ifs.close();
 }
